40728f256d2b83110756a41049457dad8fee9d8f
macgere <mac_gere@yahoo.com>
2022-08-08 18:26:22 -0500

    Updated some shtuff

---
+++ trestleBridge/Interfaces/IFeatherProducing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trestleBridge.Interfaces
+{
+    public interface IFeatherProducing
+    {
+
+        public string Type { get; set; }
+
+        public double Feather();
+    }
+}
