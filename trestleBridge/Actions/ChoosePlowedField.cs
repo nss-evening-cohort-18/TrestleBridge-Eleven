d4068ab5328a11f06752625ae69a162aa49f5d40
macgere <mac_gere@yahoo.com>
2022-08-09 20:18:21 -0500

    Changed Purchase Stock switch cases to be accurate to each animal

---
+++ trestleBridge/Actions/ChoosePlowedField.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using trestleBridge.Interfaces;
+
+namespace trestleBridge.Actions
+{
+    public class ChoosePlowedField
+    {
+        public static void CollectInput(Farm farm, IFlower flower)
+        {
+            // Console.Clear();
+            for (int i = 0; i < farm.PlowedFields.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. Natural Field");
+            }
+            Console.WriteLine();
+            // How can I output the type of animal chosen here?
+            Console.WriteLine(value: $"Place {flower.Type} where?");
+
+            Console.Write("> ");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            farm.PlowedFields[choice].AddResource(flower);
+
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
+        }
+    }
+}
--- trestleBridge/Actions/PurchaseStock.cs
+++ trestleBridge/Actions/PurchaseStock.cs
@@ -30,10 +30,11 @@ namespace trestleBridge.Actions
                     ChooseGrazingField.CollectInput(farm, new Ostrich());
                     break;
                 case 3:
-                    ChooseGrazingField.CollectInput(farm, new Chicken());
+                    //Change Duck House to Chicken House when such a thing exists
+                    ChooseDuckHouse.CollectInput(farm, new Chicken());
                     break;
                 case 4:
-                    ChooseGrazingField.CollectInput(farm, new Duck());
+                    ChooseDuckHouse.CollectInput(farm, new Duck());
                     break;
                 case 5:
                     ChooseGrazingField.CollectInput(farm, new Goat());
